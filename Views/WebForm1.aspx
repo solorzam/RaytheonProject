b37da7c9ae768426de458f3bc22d0cd9ecdf9bf1
Mauricio Solorzano <mauricio.solorzano@outlook.com>
2020-01-20 22:25:47 -0600

    Added Files

---
+++ Views/WebForm1.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="RaytheonProject.Views.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" method="get" runat="server">
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList>
+        <br />
+        <asp:GridView ID="GridView1" runat="server" BackColor="White" BorderColor="#999999" BorderStyle="Solid" BorderWidth="1px" CellPadding="3" ForeColor="Black" GridLines="Vertical" OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <AlternatingRowStyle BackColor="#CCCCCC" />
+            <FooterStyle BackColor="#CCCCCC" />
+            <HeaderStyle BackColor="Black" Font-Bold="True" ForeColor="White" />
+            <PagerStyle BackColor="#999999" ForeColor="Black" HorizontalAlign="Center" />
+            <SelectedRowStyle BackColor="#000099" Font-Bold="True" ForeColor="White" />
+            <SortedAscendingCellStyle BackColor="#F1F1F1" />
+            <SortedAscendingHeaderStyle BackColor="#808080" />
+            <SortedDescendingCellStyle BackColor="#CAC9C9" />
+            <SortedDescendingHeaderStyle BackColor="#383838" />
+        </asp:GridView>
+        <br />
+        <asp:Button ID="btnSayHello" runat="server" Text="Submit" OnClick="btnSayHello_Click1"  />
+        <asp:Literal ID="ltrMessage" runat="server"></asp:Literal>
+    </form>
+</body>
+</html>
---
+++ Views/WebForm1.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+//using RaytheonProject;
+
+namespace RaytheonProject.Views
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //txtName.Text = "";
+
+            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(CS))
+            //using (SqlConnection con = new SqlConnection("data source=DELL-TABLET\\MYPROJECTS; database=Raytheon; integrated security=SSPI"))
+            {
+                SqlCommand cmd;
+
+                if (!IsPostBack)
+                {
+                    cmd = new SqlCommand("select distinct Region from [salaries-by-region] order by Region", con);
+                    con.Open();
+
+                    DropDownList1.DataSource = cmd.ExecuteReader();
+                    DropDownList1.DataTextField = "Region";
+                    DropDownList1.DataValueField = "Region";
+                    DropDownList1.DataBind();
+                }
+                else
+                {
+                    string salaries = @"select r.School_Name,  [Region], [School_Type] , 
+                                t.[Mid_Career_Median_Salary],
+                                t.[Mid_Career_10th_Percentile_Salary],
+                                t.[Mid_Career_25th_Percentile_Salary],
+                                t.[Mid_Career_75th_Percentile_Salary],
+                                t.[Mid_Career_90th_Percentile_Salary]
+                                from    [dbo].[salaries-by-region] r, 
+                                        [dbo].[salaries-by-college-type] t
+                                where r.School_Name = t.School_Name and Region ='";
+
+                    salaries += DropDownList1.SelectedValue;
+                    salaries += "' order by r.School_Name";
+
+                    cmd = new SqlCommand(salaries, con);
+                    con.Open();
+                    SqlDataReader drSalary = cmd.ExecuteReader();
+                    GridView1.DataSource = drSalary;
+                    GridView1.DataBind();
+                }
+
+            }
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void btnSayHello_Click1(object sender, EventArgs e)
+        {
+            ltrMessage.Text = DropDownList1.SelectedValue;
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
